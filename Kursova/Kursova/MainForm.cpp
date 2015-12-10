939a4736b242a10685b1ba64ae524acc689022f0
evgenbakai <evgenbakay@mail.ru>
2015-12-10 21:16:27 +0200

    view

---
+++ Kursova/Kursova/MainForm.cpp
@@ -0,0 +1,14 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThreadAttribute]
+void main(array<String^>^ args){
+
+	Application::EnableVisualStyles;
+	Application::SetCompatibleTextRenderingDefault(false);
+	Kursova::MainForm^ form = gcnew Kursova::MainForm();
+	Application::Run(form);
+}
